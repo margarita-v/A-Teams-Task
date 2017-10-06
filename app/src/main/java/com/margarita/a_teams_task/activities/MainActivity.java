9c52dbec1315020c10bcc93c22bb09fa960548c0
Margarita <margo.himera@yandex.ru>
2017-10-06 11:42:21 +0300

    Fragments classes and layouts were added

---
+++ app/src/main/java/com/margarita/a_teams_task/activities/MainActivity.java
@@ -0,0 +1,28 @@
+package com.margarita.a_teams_task.activities;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+import com.margarita.a_teams_task.R;
+
+public class MainActivity extends AppCompatActivity
+        implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return true;
+    }
+
+}
