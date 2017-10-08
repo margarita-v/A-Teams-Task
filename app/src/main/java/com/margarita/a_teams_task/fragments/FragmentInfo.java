30755717801b840289e8f35a8ce89cdbf17e8392
Margarita <margo.himera@yandex.ru>
2017-10-08 23:40:01 +0300

    SwipeRefreshLayout was added

--- app/src/main/java/com/margarita/a_teams_task/fragments/FragmentInfo.java
+++ app/src/main/java/com/margarita/a_teams_task/fragments/FragmentInfo.java
@@ -5,6 +5,7 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.Loader;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -22,6 +23,7 @@ import java.util.List;
 
 public class FragmentInfo extends Fragment {
 
+    private SwipeRefreshLayout swipeContainer;
     private RecyclerView recyclerView;
 
     // IDs of string resources for hints in TextInputLayouts
@@ -40,6 +42,16 @@ public class FragmentInfo extends Fragment {
         View view = inflater.inflate(R.layout.fragment_info, container, false);
         recyclerView = view.findViewById(R.id.infoList);
         recyclerView.setLayoutManager(new LinearLayoutManager(container.getContext()));
+
+        swipeContainer = view.findViewById(R.id.swipeContainer);
+        swipeContainer.setColorSchemeResources(R.color.colorAccent);
+        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                items.clear();
+                performLoading(InfoLoader.LOADER_IP);
+            }
+        });
         return view;
     }
 
@@ -47,6 +59,7 @@ public class FragmentInfo extends Fragment {
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         // Start loading items consistently
+        swipeContainer.setRefreshing(true);
         performLoading(InfoLoader.LOADER_IP);
     }
 
@@ -72,12 +85,14 @@ public class FragmentInfo extends Fragment {
                     case InfoLoader.LOADER_DATETIME:
                         RecyclerViewAdapter adapter = new RecyclerViewAdapter(items.toArray(), HINTS_IDS);
                         recyclerView.setAdapter(adapter);
+                        finishLoading();
                         break;
                 }
             }
             else {
                 Toast.makeText(getContext(), "Loading error", Toast.LENGTH_SHORT).show();
                 items.clear();
+                finishLoading();
             }
         }
 
@@ -93,5 +108,12 @@ public class FragmentInfo extends Fragment {
      */
     private void performLoading(int loaderId) {
         getActivity().getSupportLoaderManager().restartLoader(loaderId, null, callbacks);
+    }
+
+    /**
+     * Perform actions after loading
+     */
+    private void finishLoading() {
+        swipeContainer.setRefreshing(false);
     }
 }
